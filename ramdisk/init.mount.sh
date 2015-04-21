b54bb1350cc3d54325ea5d0661cc0adbe83c928a
arter97 <qkrwngud825@gmail.com>
2015-04-21 17:07:26 +0900

    ramdisk: enforce discard option on f2fs partitions
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- ramdisk/init.mount.sh
+++ ramdisk/init.mount.sh
@@ -6,8 +6,8 @@ export PATH=/res/asset:$PATH
 
 mount -t ext4 -o rw,noatime,nodiratime,data=ordered,barrier=1,nodiscard /dev/block/platform/msm_sdcc.1/by-name/cache /cache
 mount -t f2fs -o rw,noatime,nodiratime,background_gc=on,nodiscard /dev/block/platform/msm_sdcc.1/by-name/cache /cache
-mount -t ext4 -o rw,noatime,nodiratime,data=ordered,barrier=1,nodiscard /dev/block/platform/msm_sdcc.1/by-name/userdata /data
-mount -t f2fs -o rw,noatime,nodiratime,background_gc=on,nodiscard /dev/block/platform/msm_sdcc.1/by-name/userdata /data
+mount -t ext4 -o rw,noatime,nodiratime,data=ordered,barrier=1,discard /dev/block/platform/msm_sdcc.1/by-name/userdata /data
+mount -t f2fs -o rw,noatime,nodiratime,background_gc=on,discard /dev/block/platform/msm_sdcc.1/by-name/userdata /data
 
 if [ -e /data/system-bind ] ; then
 	chown 0.0 /data/system-bind
@@ -16,7 +16,7 @@ if [ -e /data/system-bind ] ; then
 	mount -o remount,suid,dev /system
 else
 	mount -t ext4 -o ro,noatime,nodiratime,data=ordered,barrier=1,nodiscard /dev/block/platform/msm_sdcc.1/by-name/system /system
-	mount -t f2fs -o ro,noatime,nodiratime,background_gc=off,nodiscard /dev/block/platform/msm_sdcc.1/by-name/system /system
+	mount -t f2fs -o ro,noatime,nodiratime,background_gc=off,discard /dev/block/platform/msm_sdcc.1/by-name/system /system
 fi
 
 touch /dev/block/mounted
