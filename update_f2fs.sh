dcfac492b9c41f260c5c49b3e2e9c58c8cb3b2f0
arter97 <qkrwngud825@gmail.com>
2015-04-21 17:10:17 +0900

    update_f2fs: regenerated
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- update_f2fs.sh
+++ update_f2fs.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 git fetch f2fs
 
-find . -not -path '*/\.*' -name '*f2fs*' | grep -v 'update_f2fs\|recovery' | while read f2fs; do rm -rf $f2fs; git checkout f2fs/dev -- $f2fs; done
+find . -not -path '*/\.*' -name '*f2fs*' | grep -v 'update_f2fs\|recovery\|ramdisk' | while read f2fs; do rm -rf $f2fs; git checkout f2fs/dev -- $f2fs; done
 
 rm -rf fs/f2fs
 git checkout f2fs/dev -- fs/f2fs
 
-git log --oneline f2fs/linux-3.10 | head -1 | awk '{print $1}' | while read patch; do git cherry-pick --no-commit $patch; done
-git cherry-pick --no-commit d9d3c407cd0ee6c0f166d624d2094c9232de568b
-git cherry-pick --no-commit 86bbd59fdb8fd8835e4fd446ce11640c681860d3
+# Workaround for f2fs/linux-3.10 not being up-to-date with dev branch
+rm 0001* 2>/dev/null
+git format-patch 83dfe53c185e3554c102708c70dc1e5ff4bcac2c^..83dfe53c185e3554c102708c70dc1e5ff4bcac2c
+patch -R -p1 < 0001*
+git add fs/f2fs/acl.c
 
+git log --oneline f2fs/linux-3.10 | head -1 | awk '{print $1}' | while read patch; do git cherry-pick --no-commit $patch; done
+git cherry-pick --no-commit 4cc871a25931de58496c80ac756de8303d94122e
+git cherry-pick --no-commit 9aa487ad90c38f3a27a46bca04108b0bfb11dcb2
 git status
