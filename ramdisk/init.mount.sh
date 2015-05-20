531f4c7c108b167a677ab110c9788f364882e416
arter97 <qkrwngud825@gmail.com>
2015-05-20 22:05:39 +0900

    ramdisk: fix durp
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- ramdisk/init.mount.sh
+++ ramdisk/init.mount.sh
@@ -5,7 +5,7 @@ exec 2>&1 > /dev/kmsg
 export PATH=/res/asset:$PATH
 
 mount -t ext4 -o rw,noatime,nodiratime,data=ordered,barrier=1,nodiscard /dev/block/platform/msm_sdcc.1/by-name/cache /cache
-mount -t f2fs -o rw,noatime,nodiratime,background_gc=on,nodiscard /dev/block/platform/msm_sdcc.1/by-name/cache /cache
+mount -t f2fs -o rw,noatime,nodiratime,background_gc=on,discard /dev/block/platform/msm_sdcc.1/by-name/cache /cache
 mount -t ext4 -o rw,noatime,nodiratime,data=ordered,barrier=1,discard /dev/block/platform/msm_sdcc.1/by-name/userdata /data
 mount -t f2fs -o rw,noatime,nodiratime,background_gc=on,discard /dev/block/platform/msm_sdcc.1/by-name/userdata /data
 
